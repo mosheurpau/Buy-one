5f243b1cd0469264398dfb4c9e37cb80bedec160
mosheurpau <mosheur.pau@gamil.com>
2022-03-26 00:36:51 +0600

    create components Header and Shop

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Buy One</h1>
+            <h3>Choose 4 Watch</h3>
+        </div>
+    );
+};
+
+export default Header;
