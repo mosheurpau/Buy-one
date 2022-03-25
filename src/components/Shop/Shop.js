c5786b4cf8d59ec3c519135c57ad4c9e32149fb4
mosheurpau <mosheur.pau@gamil.com>
2022-03-26 00:50:08 +0600

    load data from json file

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect( ()=>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, [])
     return (
         <div>
-            <h3>This is shop</h3>
+            <div>
+                {
+                    products.map(product => console.log(product))
+                }
+            </div>
         </div>
     );
 };
