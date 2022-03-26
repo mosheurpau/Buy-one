7a3ad370353140f4cb669974f049138ebe38384f
mosheurpau <mosheur.pau@gamil.com>
2022-03-26 22:04:10 +0600

    added style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h1>Buy One</h1>
             <h3>Choose 4 Watch</h3>
         </div>
